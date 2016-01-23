ba03d09817131cd29057d3993ce232d6d81dacd3
Marco Chiappetta <lambdacomplete@gmail.com>
2016-01-23 13:14:26 +0200

    Add desktop notifications example

---
+++ examples/desktop_notifications/index.js
@@ -0,0 +1,19 @@
+var arper = require("arper");
+var notifier = require("node-notifier");
+
+var desktopNotificationsMiddleware = function(sender) {
+  notifier.notify({
+    title: "We detected a new node in the network",
+    message: "IP address: " + sender.ipAddr + "\nMAC address: " + sender.macAddr,
+    // icon: path.join(__dirname, "coulson.jpg"), // absolute path (not balloons)
+    sound: true, // Only Notification Center or Windows Toasters
+  });
+};
+
+arper.addMiddleware(desktopNotificationsMiddleware);
+
+arper.monitor("en0", function(err, sender) {
+  if (err) {
+    console.warn(err);
+  }
+}, true);
