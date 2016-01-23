09b6a8010485a28c34d283139c9a607f859aee89
Marco Chiappetta <lambdacomplete@gmail.com>
2016-01-23 19:00:46 +0200

    Update catch_em_all

--- examples/catch_em_all/index.js
+++ examples/catch_em_all/index.js
@@ -2,11 +2,13 @@ var arper = require("../../arper");
 var desktopNotificationsMiddleware = require("../desktop_notifications");
 var telegramMiddleware = require("../telegram");
 var openFileMiddleware = require("../welcome_home");
+var initMongodbMiddleware = require("../mongodb");
 
 // Load all middleware!
 arper.addMiddleware(desktopNotificationsMiddleware);
 arper.addMiddleware(telegramMiddleware);
 arper.addMiddleware(openFileMiddleware);
+arper.addMiddleware(initMongodbMiddleware());
 
 var boom = function(err, node) {
   if (err) {
